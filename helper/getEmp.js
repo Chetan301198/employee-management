f4cd52b02669c9d0eb9e323efeb6aa4d83d96535
Chetan Bhalerao <chetan@thefastway.in>
2023-06-24 19:40:55 +0530

    change: base url for api

--- helper/getEmp.js
+++ helper/getEmp.js
@@ -1,4 +1,4 @@
-const base_url = "http://localhost:3000";
+const base_url = "https://empfs.netlify.app";
 
 export const getUserData = async () => {
   const response = await fetch(`${base_url}/api/users`, {
